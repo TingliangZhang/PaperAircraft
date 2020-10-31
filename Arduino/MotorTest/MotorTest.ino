e1b5155ebfe56ab408f6c2162b2b7da0290e285b
Tingliang Zhang <zhangtl16@mails.tsinghua.edu.cn>
2020-10-31 18:19:03 +0800

    Create MotorTest.ino

---
+++ Arduino/MotorTest/MotorTest.ino
@@ -0,0 +1,30 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Sweep
+*/
+
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+void setup() {
+  myservo.attach(10);  // attaches the servo on pin 10 to the servo object
+  myservo.write(180);              // tell servo to go to position in variable 'pos'
+  delay(2000);                       // waits 1000ms
+}
+
+void loop() {
+
+//    myservo.write(180);              // tell servo to go to position in variable 'pos'
+//    delay(1000);                       // waits 1000ms
+//    myservo.write(0);              // tell servo to go to position in variable 'pos'
+//    delay(1000);                       // waits 1000ms
+
+}

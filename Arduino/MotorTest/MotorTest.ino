9ff1c28cb8c229b5964041888496627da7395bdd
Tingliang Zhang <zhangtl16@mails.tsinghua.edu.cn>
2020-11-01 16:33:45 +0800

    Update MotorTest.ino

--- Arduino/MotorTest/MotorTest.ino
+++ Arduino/MotorTest/MotorTest.ino
@@ -12,19 +12,11 @@
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
-int pos = 0;    // variable to store the servo position
-
 void setup() {
-  myservo.attach(10, 900, 2100);  // attaches the servo on pin 10 to the servo object
-  myservo.write(90);              // tell servo to go to position in variable 'pos'
-//  delay(2000);                       // waits 1000ms
+  myservo.attach(10, 1200, 2200);  // attaches the servo on pin 10 to the servo object
 }
 
 void loop() {
-
-//    myservo.write(180);              // tell servo to go to position in variable 'pos'
-//    delay(1000);                       // waits 1000ms
-//    myservo.write(0);              // tell servo to go to position in variable 'pos'
-//    delay(1000);                       // waits 1000ms
+  myservo.write(120);              // tell servo to go to position
 
 }
